748d860a90d52937927c13687d8f582d68996836
danilo <daniel.malugo@gmail.com>
2020-07-10 20:25:10 -0500

    added letters function

--- src/abecedario.cpp
+++ src/abecedario.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
 #include <array>
+#include "CustomChar.hpp"
 
-
-struct CustomChar{
-    int ancho;
-	std::string linea1;
-	std::string linea2;
-	std::string linea3;
-	std::string linea4;
-	std::string linea5;
-	std::string linea6;
-	std::string linea7;
-};
-
-int main (){
+int Letters (){
 
 
 CustomChar a {};
