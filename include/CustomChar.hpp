6f3190da9c6050dd4e8806bdfed762896807e160
danilo <daniel.malugo@gmail.com>
2020-07-10 19:07:43 -0500

    added struct

---
+++ include/CustomChar.hpp
@@ -0,0 +1,16 @@
+#ifndef CUSTOMCHAR_H
+#define CUSTOMCHAR_H
+
+#include<string>
+
+struct CustomChar{
+	std::string linea1;
+	std::string linea2;
+	std::string linea3;
+	std::string linea4;
+	std::string linea5;
+	std::string linea6;
+	std::string linea7;
+};
+
+#endif
