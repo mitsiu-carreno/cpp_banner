b4109f1eaa37eea7d8eb60ad8683cbcb5794273a
mitsiu <mitsiu.carreno@gmail.com>
2020-07-14 20:18:34 -0500

    validate input

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,33 @@
-#include <iostream>
-#include <string>
+#include "utils.hpp"
 #include "abecedario.hpp"
 
+#include <iostream>
+#include <string>
+
+
 int main(){
-  
-  std::string input {"aei"};
-  std::cout << &input << "\n";
-  
-  for(char e : input){
-    std::cout << e << "\n";
+
+  std::string input;
+
+  while(true){  
+    std::cout << "Ingresa un texto: ";
+    // Get line and put it into input
+    std::getline(std::cin, input);
+    
+    char non_valid_char {utils::InvalidCharacters(input)};
+    // -1 is known to mean every char is valid
+    if(non_valid_char == -1){
+      break;
+    }else{
+      std::cout << "Lo sentimos el caracter " << non_valid_char << " no esta permitido\n";
+    }
   }
+
+  for(char letter : input){
+    std::cout << letter << "\n";
+    // PrintLetters(letter);
+  }
+
   
   PrintLetters();
   
--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -1,15 +1,35 @@
 #include<iostream>
+#include <string>
 
-bool InputCheck () {
-	if(std::cin.fail()){
-		std::cin.clear();
-		std::cin.ignore(32767, '\n');
-		return false;
-	
-	}else{
-		std::cin.ignore(32767,'\n');
-		return true;
-	
-	}
+namespace utils{
 
+  bool InputCheck () {
+	  if(std::cin.fail()){
+		  std::cin.clear();
+		  std::cin.ignore(32767, '\n');
+		  return false;
+	
+	  }else{
+		  std::cin.ignore(32767,'\n');
+		  return true;
+	
+	  }
+
+  }
+
+  char InvalidCharacters(std::string &input){
+  
+    for(char &letter : input){
+      if(letter >= 'A' && letter <= 'Z'){
+        // Turn uppercase letter to lowercase
+        letter = letter + 32;
+      }
+      if(letter != ' ' && (letter < 'a' || letter > 'z')){
+        // Anything outside lowercase alphabet is invalid
+        return letter;  
+      }
+    }
+    // All characters are valid
+    return -1;
+  }
 }
