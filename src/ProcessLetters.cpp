23c827ef3996417e218b62393dd24643c90af970
danilo <daniel.malugo@gmail.com>
2020-07-24 19:26:55 -0500

    abc

--- src/ProcessLetters.cpp
+++ src/ProcessLetters.cpp
@@ -26,11 +26,65 @@ std::array <const std::string, constants::rows_per_letter> SelectLetters (char l
 		case 'f':
 			return ABC::F;
 			break;
+		case 'g':
+			return ABC::G;
+			break;
+		case 'h':
+			return ABC::H;
+			break;
 		case 'i':
 			return ABC::I;
 			break;
+		case 'j':
+			return ABC::J;
+			break;
+		case 'k':
+			return ABC::K;
+			break;
+		case 'l':
+			return ABC::L;
+			break;
+		case 'm':
+			return ABC::M;
+			break;
+		case 'n':
+			return ABC::N;
+			break;
 		case 'o':
 			return ABC::O;
+			break;
+		case 'p':
+			return ABC::P;
+			break;
+		case 'q':
+			return ABC::Q;
+			break;
+		case 'r':
+			return ABC::R;
+			break;
+		case 's':
+			return ABC::S;
+			break;
+		case 't':
+			return ABC::T;
+			break;
+		case 'u':
+			return ABC::U;
+			break;
+		case 'v':
+			return ABC::V;
+			break;
+		case 'w':
+			return ABC::W;
+			break;
+		case 'x':
+			return ABC::X;
+			break;
+		case 'y':
+			return ABC::Y;
+			break;
+		case 'z':
+			return ABC::Z;
 			break;
     default:
       return ABC::Z;
